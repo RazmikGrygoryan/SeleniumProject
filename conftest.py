9577a83ca829d46ee0d7c65dc2b51d791a4ccf22
RazmikGrygoryan <razmik97@list.ru>
2024-05-11 16:44:54 +0300

    text

--- conftest.py
+++ conftest.py
@@ -11,6 +11,8 @@ def driver():
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_driver = webdriver.Chrome(options=options)
     return chrome_driver
 
