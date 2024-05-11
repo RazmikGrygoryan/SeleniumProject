faafd490d8098f9762c4f6d4b7a9f84594b353c3
RazmikGrygoryan <razmik97@list.ru>
2024-05-11 16:28:15 +0300

    text

--- conftest.py
+++ conftest.py
@@ -9,8 +9,8 @@ from pages.sale_page import Sales
 @pytest.fixture()
 def driver():
     options = Options()
-    # options.add_argument("--headless")
-    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--headless")
+    # options.add_argument("--window-size=1920,1080")
     chrome_driver = webdriver.Chrome(options=options)
     return chrome_driver
 
